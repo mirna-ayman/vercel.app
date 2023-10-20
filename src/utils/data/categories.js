505d99bcd11280f5d8e2b678281061aed5316066
mirna-ayman <86626105+mirna-ayman@users.noreply.github.com>
2023-10-20 21:28:31 +0300

    Add files via upload

---
+++ src/utils/data/categories.js
@@ -0,0 +1,95 @@
+const ButtonLinks = [
+  {
+    id: 1,
+    name: 'All',
+    category: 'all',
+    icon: 'ri-briefcase-line',
+  },
+  {
+    id: 2,
+    name: 'Remote Jobs',
+    category: 'remote',
+    icon: 'ri-remote-control-line',
+  },
+  {
+    id: 3,
+    name: 'Resume Builder',
+    category: 'resume',
+    icon: 'ri-clipboard-line',
+  },
+  {
+    id: 4,
+    name: 'AI',
+    category: 'ai',
+    icon: 'ri-robot-line',
+  },
+  {
+    id: 5,
+    name: 'Ethical Hacking',
+    category: 'ethical',
+    icon: 'ri-bug-line',
+  },
+  {
+    id: 6,
+    name: 'Movies | Series',
+    category: 'movies',
+    icon: 'ri-movie-2-line',
+  },
+  {
+    id: 7,
+    name: 'Extensions',
+    category: 'extensions',
+    icon: 'ri-chrome-line',
+  },
+  {
+    id: 8,
+    name: 'UI Design',
+    category: 'UI',
+    icon: 'ri-tools-fill',
+  },
+  {
+    id: 9,
+    name: 'Frontend Tools',
+    category: 'web',
+    icon: 'ri-tools-fill',
+  },
+  {
+    id: 10,
+    name: 'Coding Platforms',
+    category: 'coding',
+    icon: 'ri-tools-fill',
+  },
+  {
+    id: 11,
+    name: 'Course Platforms',
+    category: 'courses',
+    icon: 'ri-tools-fill',
+  },
+  {
+    id: 12,
+    name: 'Image generation',
+    category: 'image_generation',
+    icon: 'ri-tools-fill',
+  },
+  {
+    id: 13,
+    name: 'API',
+    category: 'api',
+    icon: 'ri-tools-fill',
+  },
+  {
+    id: 14,
+    name: 'eSports',
+    category: 'eSports',
+    icon: 'ri-tools-fill',
+  },
+  {
+    id: 15,
+    name: 'Useful Tools',
+    category: 'tools',
+    icon: 'ri-tools-fill',
+  },
+
+]
+
+export default ButtonLinks
---
+++ src/utils/data/filters.js
@@ -0,0 +1,17 @@
+export const allFilters = [
+  'all',
+  'remote',
+  'resume',
+  'ai',
+  'ethical',
+  'movies',
+  'extensions',
+  'ui',
+  'web',
+  'coding',
+  'courses',
+  'tools',
+  'image_generation',
+  'api',
+  'esports'
+]
